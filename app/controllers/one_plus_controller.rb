ab110c04c661928852f82eed072f598f49fbe693
akshayparekh7 <akshayparekh1497@gmail.com>
2020-08-14 22:56:40 +0530

    added title logo and few updates to one plus controller

--- app/controllers/one_plus_controller.rb
+++ app/controllers/one_plus_controller.rb
@@ -2,9 +2,10 @@ class OnePlusController < ApplicationController
 
   def index
     @one_plus_specifications = Specification.where(phone_name: "One plus")
+    @one_plus_specifications = @one_plus_specifications.order(created_at: :desc)
   end  
 
   def show
-
-  end  
+    @specification = Specification.find(params[:id])
+  end 
 end
