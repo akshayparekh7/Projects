432be0862f7c4d75ee5510b60cf44d837fedad75
akshayparekh7 <akshayparekh1497@gmail.com>
2020-08-17 13:25:23 +0530

    Added popup carousel

--- app/controllers/one_plus_controller.rb
+++ app/controllers/one_plus_controller.rb
@@ -1,12 +1,14 @@
 class OnePlusController < ApplicationController
 
   def index
-    @one_plus_specifications = Specification.where(phone_name: "One plus")
+    @one_plus_specifications = Specification.all
+    # @one_plus_specifications = Specification.where(phone_name: "One plus")
     @one_plus_specifications = @one_plus_specifications.order(created_at: :desc)
   end  
 
   def show
     @specification = Specification.find(params[:id])
-    @images = Dir.glob('app/assets/images/*')
+    @model_folder = @specification.phone_name.downcase.gsub(' ', '_')
+    @images = Dir.glob("app/assets/images/#{@model_folder}/*")
   end 
 end
