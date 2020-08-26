54d10506b85a2a224b5374f8f0f67f2e590877f5
akshayparekh7 <akshayparekh1497@gmail.com>
2020-08-26 16:47:34 +0530

    Edited nokogiri body

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -2,13 +2,15 @@ class Specification < ApplicationRecord
     def scrap
         url = "https://www.gsmarena.com/oneplus-phones-95.php"
         unparsed = HTTParty.get(url)
-        parsed = Nokogiri::HTML(unparsed)
+        parsed = Nokogiri::HTML(unparsed.body)
         a = parsed.css('div.makers')
         a.css('ul').each do |li|
             li.css('span').each do |phone|
               b = phone.text
-              c = Specification.new(phone_name: b)
-              c.save
+              if Specification.where(phone_name: b).blank?
+                c = Specification.new(phone_name: b)
+                c.save
+              end
             end
         end  
     end
