86b20d3541228b57b79b87329258926325f80b29
akshayparekh7 <akshayparekh1497@gmail.com>
2020-08-29 23:08:55 +0530

    Created a generic index template

--- app/controllers/one_plus_controller.rb
+++ app/controllers/one_plus_controller.rb
@@ -1,14 +1,35 @@
 class OnePlusController < ApplicationController
 
+  helper_method  :set_action_path
+
   def index
-    @one_plus_specifications = Specification.where(brand_name: "One Plus")
-    @one_plus_specifications = @one_plus_specifications.order(launch_date: :desc)
-    @index_images = Dir.glob("app/assets/images/index_images/one_plus/*")
-  end  
+    create_list_for_index
+    set_images
+  end
 
   def show
-    @specification = Specification.find(params[:id])
+    @specification = OnePlus.find(params[:id])
+    set_modal_images
+  end
+
+  private
+
+  def create_list_for_index
+    @specifications = OnePlus.where(brand_name: "One Plus")
+    @specifications = @specifications.order(launch_date: :desc)
+  end
+
+  def set_action_path
+    polymorphic_path(one_plu_path)
+  end
+
+  def set_images
+    @index_images = Dir.glob("app/assets/images/index_images/one_plus/*")
+  end
+
+  def set_modal_images
     @model_folder = @specification.phone_name.downcase.gsub(' ', '_')
     @images = Dir.glob("app/assets/images/#{@model_folder}/*")
-  end 
+  end
+
 end
